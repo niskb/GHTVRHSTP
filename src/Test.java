42b24172dfcbb602d546697b1d6b425cd511c535
Brian Niski <niskb19@gmail.com>
2022-12-11 10:19:46 -0500

    Parse a XML File
    Parses the first XML file in the directory.

--- src/Test.java
+++ src/Test.java
@@ -4,10 +4,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class Test {
 
 	private static String[] directories = new String[578]; // 578 IDs in TRACKS_CLEAN
-	private static int count = 0; // Should be 577 in the end of the scan
+	private static int count = 0; // Should be 578 in the end of the scan
 
 	public static void main(String[] args) {
 		String directoryPath = "OVERRIDE\\TRACKS_CLEAN";
@@ -15,10 +25,10 @@ public class Test {
 		File[] files = directory.listFiles();
 
 		// Search through directories and print in a tabbed format
-		System.out.println("Run printFileName(...)");
-		for (File file : files) {
-			printFileName(file, 0);
-		}
+//		System.out.println("Run printFileName(...)");
+//		for (File file : files) {
+//			printFileName(file, 0);
+//		}
 
 		System.out.println("Run scanDirectories(...)");
 		// For setting up directories
@@ -26,18 +36,22 @@ public class Test {
 			scanDirectories(file, 0);
 		}
 
-		System.out.println("Run printDirectories(...)");
+//		System.out.println("Run printDirectories(...)");
 
 		System.out.println("Files counted: " + count);
 
 		// Print All Directories
-		printDirectories();
+//		printDirectories();
 
-		System.out.println("Print a Random File's contents with printFileContents(...)");
+//		System.out.println("Print a Random File's contents with printFileContents(...)");
 		// Now read a random File's contents
-		int randomDirectory = (int) (Math.random() * directories.length);
-		System.out.println("Random File Number: " + (randomDirectory + 1));
-		printFileContents(randomDirectory);
+//		int randomDirectory = (int) (Math.random() * directories.length);
+//		System.out.println("Random File Number: " + (randomDirectory + 1));
+//		printFileContents(randomDirectory);
+
+		// We need to parse each file XML
+
+		parser();
 
 	}
 
@@ -99,4 +113,58 @@ public class Test {
 		}
 	}
 
+	private static void parser() {
+
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			File file = new File(directories[0]);
+			Document doc = builder.parse(file);
+			doc.getDocumentElement().normalize();
+
+			System.out.println("\nWe are printing out the song data for the first file in the directory list...\n"
+					+ directories[0] + "\n");
+
+			System.out.println("Root Element : " + doc.getDocumentElement().getNodeName());
+			System.out.println("-----");
+
+			// Get highway speed data
+			NodeList children = doc.getChildNodes();
+			Element trackElement = (Element) children.item(0);
+			System.out.println("Element Name: " + trackElement.getNodeName());
+
+			// The highway element is the second node in Track
+			// If we want all of the node data, we need to look at the trackChildren length
+			NodeList trackChildren = trackElement.getChildNodes();
+			Element highwayElement = (Element) trackChildren.item(1);
+			System.out.println("Child Element Name: " + highwayElement.getNodeName());
+
+			// Obtain data
+			String newbeginner = highwayElement.getAttribute("newbeginner");
+			System.out.println("newbeginner: " + newbeginner);
+
+			String neweasy = highwayElement.getAttribute("neweasy");
+			System.out.println("newneweasy: " + neweasy);
+
+			String newmedium = highwayElement.getAttribute("newmedium");
+			System.out.println("newmedium: " + newmedium);
+
+			String newhard = highwayElement.getAttribute("newhard");
+			System.out.println("newhard: " + newhard);
+
+			String newexpert = highwayElement.getAttribute("newexpert");
+			System.out.println("newexpert: " + newexpert);
+
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+			System.out.println("Fail");
+		} catch (SAXException e) {
+			e.printStackTrace();
+			System.out.println("Fail");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Fail");
+		}
+	}
 }
